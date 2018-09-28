d8f11e1f86f22b86f561f65da5ea2254856974a8
bielus789 <bielus@onet.pl>
2018-09-28 13:20:33 +0200

    my second repository

---
+++ jedynka/index.php
@@ -0,0 +1,49 @@
+﻿<html>
+<head></head>
+<body>
+
+<?php
+
+$array = array();
+$arrayLength = rand(10,60);
+//echo $arrayLength;
+for($i = 0; $i < $arrayLength; $i++){
+  array_push($array, rand(1,100));
+}
+
+
+
+
+$average = array_sum($array)/$arrayLength;
+echo ~~$average;
+echo "</br>";
+echo "to jest średnia wartość z elementów tablicy";
+echo "</br>";
+
+
+
+function fib($n)
+{
+   if($n == 0) return 0;
+   else if ($n == 1) return 1;
+   else
+   {
+      return fib($n-1) + fib($n-2);
+   }
+}
+
+
+$last = array_pop($array);
+echo "</br>";
+echo "Ciag fibonacciego dla ostatniego elementu tablicy = ";
+echo fib($last);
+
+?>
+
+<div class=".$arrayLength.">jakto</div><div class="column2">jakto</div><div class="column3">jakto</div>
+
+</body>
+</html>
+
+<?php
+
