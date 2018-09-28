a1addc59a3a2188824a2fc0dd66d2e2bea975e4e
bielus789 <bielus@onet.pl>
2018-09-28 13:16:29 +0200

    my first repository

---
+++ dwojka/theme.js
@@ -0,0 +1,5 @@
+﻿$(document).ready(function(){
+      $.ajax({url: "http://test.projets.pl/rekrutacja/php/data.json", success: function(result){
+        var data = JSON.parse(result);
+    }});
+})
